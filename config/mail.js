7192fd4ddc150a951552c02953bdd39433de1f56
honglio <hong.liu@live.cn>
2014-05-13 09:26:07 +0800

    init

---
+++ config/mail.js
@@ -0,0 +1,15 @@
+// Copy in your particulars and rename this to mail.js
+module.exports = {
+  service: "mxhichina",
+  host: "smtp.mxhichina.com",
+  port: 587,
+  secureConnection: false,
+  //name: "servername",
+  auth: {
+    user: "info",
+    pass: "12345china"
+  },
+  ignoreTLS: false,
+  debug: false,
+  maxConnections: 5 // Default is 5
+}
